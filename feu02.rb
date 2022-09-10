11a046e30f2dac89453fbf8a31798037529cd6ea
Magapo85 <petithomme.guillaume@gmail.com>
2022-09-10 23:27:10 +0200

    exo 2 suite 4

--- feu02.rb
+++ feu02.rb
@@ -14,6 +14,21 @@ def longueurArgument(mot)
         return i
 end
 
+#fonction pour trouver un caractere dans un array, comme .index()
+def trouverDansArray(carac, array)
+        sortie = "ok"
+        index = longueurArgument(array)
+        i = 0
+        while i < longueurArgument(array) && sortie == "ok"
+                if array[i] == carac
+                        sortie = "erreur"
+                        index = i
+                end
+                i = i + 1
+        end
+        return index, sortie
+end
+
 #fonction utile qui verifie la syntaxe du calcul
 def testArgument(array, arrayCheck)
         i = 0
@@ -42,26 +57,49 @@ def testArgument(array, arrayCheck)
 		i = i + 1
 	end
 	i = 0
-	new = []
+	new = ""
 	while array[i] && sortie == "ok"
 		if array[i] != " "
-			new.push(array[i])
+			new = new + array[i]
 		end
 	end
-	i = 1
+	i = 0
 	while new[i] && sortie == "ok"
-		
+		if (i == 0 && trouverDansArray(new[i], arrayCheck[2..8])[1] == "erreur") || (!new[i+1] && trouverDansArray(new[i], arrayCheck[3..8].push(arrayCheck[1]))[1] == "erreur")
+			sortie = "erreur"
+		else
+			if new[i] == "(" && (trouverDansArray(new[i-1], arrayCheck[8..-1])[1] == "erreur" || trouverDansArray(new[i+1], arrayCheck[2..8])[1] == "erreur")
+                                sortie == "erreur"
+                        end
+                        if new[i] == ")" && (trouverDansArray(new[i-1], arrayCheck[1..8].delete_at(1))[1] == "erreur" || trouverDansArray(new[i+1], arrayCheck[8..-1])[1] == "erreur")
+                                sortie == "erreur"
+                        end
+			if (new[i] == "+" || new[i] == "-" || new[i] == "*" || new[i] == "/" || new[i] == "%") && (trouverDansArray(new[i-1], arrayCheck[1..8].delete_at(1))[1] == "erreur" || trouverDansArray(new[i+1], arrayCheck[2..8])[1] == "erreur")
+                                sortie == "erreur"
+                        end
+			if new[i] == "." && (trouverDansArray(new[i-1], arrayCheck[1..8])[1] == "erreur" || trouverDansArray(new[i+1], arrayCheck[1..8])[1] == "erreur")
+                                sortie == "erreur"
+                        end
+		end
 	end
 	if count != 0 && sortie == "ok"
 		sortie = "erreur"
 	end
-        return sortie
+        return sortie, new
 end
 
-goodSyntaxElement = [" ", ".", "(", ")", "+", "-", "*", "/", "%", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+def calcul(formule)
+	resultat = 0
+
+	return resultat
+end
+
+goodSyntaxElement = [" ", "(", ")", "+", "-", "*", "/", "%", ".", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 #coeur du programme
-if longueurArgument(ARGV) != 1 || longueurArgument(ARGV[0]) < 3 || testArgument(ARGV[0], goodSyntaxElement) == "erreur"
+sortie, new = testArgument(ARGV[0], goodSyntaxElement)
+if longueurArgument(ARGV) != 1 || longueurArgument(ARGV[0]) < 3 || sortie == "erreur"
 	puts "error"
 else
-	puts "calcul en cours"
+	puts "calcul en cours..."
+	puts new + " = " + calcul(new)
 end
